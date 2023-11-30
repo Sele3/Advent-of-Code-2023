3a51735eaeeb35d30c3f9039dddbe92bcaf62033
Sele3 <STAN266@e.ntu.edu.sg>
2023-12-01 00:04:12 +0800

    refactor: change day number naming convention

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import importlib
 
+DAY_COUNT = 1
 
-def create_solver(day_number, input_file):
-    module_name = f"day{day_number}.solver"
+
+def create_solver(day_str: str, input_file):
+    module_name = f"day{day_str}.solver"
 
     try:
         module = importlib.import_module(module_name)
         solver_class = getattr(module, "Solver")
         return solver_class(input_file)
     except (ModuleNotFoundError, AttributeError):
-        raise ValueError(f"No solver implemented for day {day_number}")
+        raise ValueError(f"No solver implemented for day {day_str}")
 
 
 if __name__ == "__main__":
-    day_number = 1
-    input_file = f"day{day_number}/input.txt"
+    day_str = str(DAY_COUNT).zfill(2)
+    input_file = f"day{day_str}/input.txt"
 
     try:
-        solver = create_solver(day_number, input_file)
+        solver = create_solver(day_str, input_file)
         solver.read_input_file()
     except Exception as e:
         print(e)
