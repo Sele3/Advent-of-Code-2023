d52b647bd8df16225aa973fcdb5d2dcbf290c350
Sele3 <STAN266@e.ntu.edu.sg>
2023-12-05 13:57:02 +0800

    refactor: use set intersection to get number of matches

--- day04/utils.py
+++ day04/utils.py
@@ -7,22 +7,14 @@ class ScratchCard:
     winning_numbers: List[int]
     card_numbers: List[int]
 
-    # Helper attributes
-    all_numbers: List[bool] = field(
-        init=False, default_factory=lambda: [False] * 100
-    )  # Stores all numbers on the card
-
     # Solution-specific attributes
     points: int = field(init=False)
 
     def __post_init__(self):
-        for number in self.card_numbers:
-            self.all_numbers[number] = True
-
         self.points = self._calculate_points()
 
     def get_number_of_matches(self) -> int:
-        return sum(self.all_numbers[number] for number in self.winning_numbers)
+        return len(set(self.winning_numbers) & set(self.card_numbers))
 
     def _calculate_points(self) -> int:
         matches = self.get_number_of_matches()
