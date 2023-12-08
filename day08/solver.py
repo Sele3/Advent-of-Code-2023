1c09af5a7a8eb7abd05a3c9ec5c4088c1b45543f
Sele3 <STAN266@e.ntu.edu.sg>
2023-12-08 14:38:23 +0800

    feat: day 8

--- day08/solver.py
+++ day08/solver.py
@@ -1,12 +1,32 @@
+import re
+from typing import Dict
+
 from advent_of_code_solver import BaseSolver
+
+from .utils import MultiStartNavigator, Node, SingleStartNavigator
+
+NETWORK: Dict[str, Node] = {}
 
 
 class Solver(BaseSolver):
     def parse_input(self, file):
-        pass
+        instructions, nodes = file.read().split("\n\n")
+
+        # Construct the network dictionary
+        for line in nodes.splitlines():
+            name, left, right = re.findall(r"[A-Z]{3}", line)
+            NETWORK[name] = Node(name, left, right)
+
+        return instructions
 
     def solve_part1(self):
-        pass
+        navigator = SingleStartNavigator(self.input, NETWORK)
+        return navigator.get_steps_to_reach()
 
     def solve_part2(self):
-        pass
+        nodes = list(NETWORK.keys())
+        sources = [node for node in nodes if node[-1] == "A"]
+        destinations = [node for node in nodes if node[-1] == "Z"]
+
+        navigator = MultiStartNavigator(self.input, NETWORK, sources, destinations)
+        return navigator.get_steps_to_reach()
---
+++ day08/utils.py
@@ -0,0 +1,84 @@
+from collections import namedtuple
+from math import lcm
+from typing import Dict, List
+
+Node = namedtuple("Node", ("name", "left", "right"))
+
+
+class SingleStartNavigator:
+    """
+    A class that navigates a network of nodes starting from a single source node.
+    """
+
+    def __init__(self, instructions: str, network: Dict[str, Node]):
+        self.instructions = instructions
+        self.network = network
+        self.current_node = "AAA"
+
+    def get_steps_to_reach(self) -> int:
+        """
+        Returns the number of steps required to reach the destination node.
+        """
+        idx = steps = 0
+
+        while True:
+            steps += 1
+            self._update_node_position(self.instructions[idx])
+
+            if self._has_reached_destination():
+                break
+
+            idx = (idx + 1) % len(self.instructions)
+
+        return steps
+
+    def _update_node_position(self, direction: str) -> None:
+        """
+        Updates the current node position based on the direction.
+        :param direction: Either "L" or "R"
+        """
+        node = self.network[self.current_node]
+        self.current_node = node.left if direction == "L" else node.right
+
+    def _has_reached_destination(self) -> bool:
+        """
+        Returns True if the current node is the destination node.
+        """
+        return self.current_node == "ZZZ"
+
+
+class MultiStartNavigator(SingleStartNavigator):
+    """
+    A class that navigates a network of nodes starting from multiple source nodes.
+    """
+
+    def __init__(
+        self,
+        instructions: str,
+        network: Dict[str, Node],
+        sources: List[str],
+        destinations: List[str],
+    ):
+        super().__init__(instructions, network)
+        self.sources = sources
+        self.destinations = set(destinations)
+
+    def get_steps_to_reach(self) -> int:
+        """
+        Returns the number of steps required for all source nodes to reach the destination nodes at the same time.
+        The number of steps is the LCM of the number of steps required for each source node to reach the destination.
+        """
+        steps_list = []
+
+        for source_node in self.sources:
+            self.current_node = source_node
+            steps = super().get_steps_to_reach()
+            steps_list.append(steps)
+
+        return lcm(*steps_list)
+
+    def _has_reached_destination(self) -> bool:
+        """
+        Returns True if the current node is one of the destination nodes.
+        """
+        return self.current_node in self.destinations
