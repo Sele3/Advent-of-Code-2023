d4c646917496b900a329d0fa2ded38c08a96f3c9
Sele3 <STAN266@e.ntu.edu.sg>
2023-12-09 13:16:57 +0800

    feat: day 9

--- day09/solver.py
+++ day09/solver.py
@@ -1,12 +1,25 @@
+from typing import List
+
 from advent_of_code_solver import BaseSolver
+
+
+def extrapolate(nums: List[int]) -> int:
+    """
+    Extrapolate a list of numbers by adding the difference between each number
+    """
+    if all(num == 0 for num in nums):
+        return 0
+
+    diffs = [nums[i] - nums[i - 1] for i in range(1, len(nums))]
+    return nums[-1] + extrapolate(diffs)
 
 
 class Solver(BaseSolver):
     def parse_input(self, file):
-        pass
+        return [list(map(int, line.split())) for line in file.readlines()]
 
     def solve_part1(self):
-        pass
+        return sum(extrapolate(nums) for nums in self.input)
 
     def solve_part2(self):
-        pass
+        return sum(extrapolate(nums[::-1]) for nums in self.input)
