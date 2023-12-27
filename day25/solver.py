d87d7092c4023e1b37409c0661f85b37b63abe4f
Sele3 <STAN266@e.ntu.edu.sg>
2023-12-28 02:43:03 +0800

    feat: day 25

--- day25/solver.py
+++ day25/solver.py
@@ -1,14 +1,38 @@
-from advent_of_code_solver import BaseSolver
+from collections import defaultdict
+from typing import Dict, List
 
-from .utils import *
+from advent_of_code_solver import BaseSolver
 
 
 class Solver(BaseSolver):
     def parse_input(self, file):
-        pass
+        # Build a graph from the input
+        result = defaultdict(list)
+        for line in file.read().splitlines():
+            curr, others = line.split(": ")
+            for other in others.split(" "):
+                result[curr].append(other)
+                result[other].append(curr)
+
+        return result
 
     def solve_part1(self):
-        pass
+        g: Dict[str, List[str]] = self.input
+        not_connected = {k: 0 for k in g.keys()}
+        connected = set()
+        total = 0
+
+        while total != 3:
+            most_common = max(not_connected, key=not_connected.get)
+            connected.add(most_common)
+            total -= not_connected.pop(most_common)
+
+            for v in g[most_common]:
+                if v in not_connected:
+                    not_connected[v] += 1
+                    total += 1
+
+        return len(not_connected) * len(connected)
 
     def solve_part2(self):
-        pass
+        return "Merry Christmas!"
